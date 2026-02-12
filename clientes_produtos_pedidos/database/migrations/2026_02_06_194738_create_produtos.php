952ff00ee666a8f1a7348425a7200f70862a691d
Marcos Barros <115720083+MarcosBarros1@users.noreply.github.com>
2026-02-12 16:47:24 -0300

    Add files via upload

---
+++ clientes_produtos_pedidos/database/migrations/2026_02_06_194738_create_produtos.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('categoria_id')->constrained('categorias')->cascadeOnDelete();
+            $table->string('nome');
+            $table->float('preco');
+            $table->integer('quantidade');
+            $table->boolean('ativo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('produtos', function (Blueprint $table) {
+            // Remove a FK e a coluna caso desfaça a migration
+            $table->dropForeign(['categoria_id']);
+            $table->dropColumn('categoria_id');
+        });
+    }
+};
